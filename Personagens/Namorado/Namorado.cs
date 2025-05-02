11870e833bdc0a1206ac1cb7fa06d47c269036bf
Fidel C.S. Pereira <cspereira2000@gmail.com>
2025-05-01 22:29:25 -0300

    Namorado melhor desenvolvido ainda (tá virando um bom conjuge pra mina)

--- Personagens/Namorado/Namorado.cs
+++ Personagens/Namorado/Namorado.cs
@@ -13,24 +13,39 @@ public partial class Namorado : CharacterBody2D
 	private int Lifes {get; set;} = 5;
 	private int Gravity { get; set; } = GravNum;
 	private int Speed { get; set; } = Velo;
-	private int Direction {get; set; } = 1;
-	private bool IsImmune {get; set; } = false;
-	private bool IsJumping {get; set; } = false;
-	private bool BackupOnFloor {get; set; } = true;
-	private bool IsDashing {get; set; } = false;
+	private int Direction { get; set; } = 1;
+	private bool IsImmune { get; set; } = false;
+	private bool IsJumping { get; set; } = false;
+	private bool BackupOnFloor { get; set; } = true;
+	private bool IsDashing { get; set; } = false;
+	private bool IsPunching { get; set; } = false;
 	
 	/*nós*/
 	private Timer TimerImunidade;
 	private Timer TimerDash; 
 	private Timer TimerPulo;  
+	private Area2D Soco;
+	private Timer TimerSoco;
+	private AnimatedSprite2D Sprite;
 	
 	public override void _Ready(){
+		/*timer que cuida da imunidade*/
 		TimerImunidade = GetNode<Timer>("TimerImunidade");
 		TimerImunidade.Timeout += FimDaImunidade;
+		/*timer que cuida do dash*/
 		TimerDash = GetNode<Timer>("TimerDash");
 		TimerDash.Timeout += FimDoDash;
+		/*timer que cuida do pulo*/
 		TimerPulo = GetNode<Timer>("TimerPulo");
 		TimerPulo.Timeout += FimDoPulo;
+		/*area2D e timer que cuida do soco*/
+		Soco = GetNode<Area2D>("Soco");
+		Soco.BodyEntered += SocoColisao; 
+		Soco.Monitoring = false;
+		TimerSoco = GetNode<Timer>("TimerSoco");
+		TimerSoco.Timeout += FimDoSoco;
+		/*sprite animado*/
+		Sprite = GetNode<AnimatedSprite2D>("SpriteNamorado");
 		GD.Print("Vidas restantes: " + Lifes);
 	}
 	
@@ -42,6 +57,26 @@ public partial class Namorado : CharacterBody2D
 		if(Colisao != null){
 			AdministraColisao(Colisao.GetCollider() as Node2D);
 		}
+		Bate();
+	}
+	
+	private void Gravidade(){
+		Velocity = new Vector2(0, Gravity);
+		MoveAndSlide();
+	}
+	
+	private void Movimenta(){
+		Vector2 inputDirection = Input.GetVector("left", "right", "down", "up");
+		if(!IsDashing){
+			if(inputDirection.X != 0){
+				Direction = (int)inputDirection.X;
+			}
+			Velocity = inputDirection * Speed;
+			/*MoveAndSlide();*/
+		}
+		
+		Dash();
+		Pula();
 	}
 	
 	private void AdministraColisao(Node2D Colisor){
@@ -62,25 +97,6 @@ public partial class Namorado : CharacterBody2D
 	
 	private void FimDaImunidade(){
 		IsImmune = false;
-	}
-	
-	private void Gravidade(){
-		Velocity = new Vector2(0, Gravity);
-		MoveAndSlide();
-	}
-	
-	private void Movimenta(){
-		Vector2 inputDirection = Input.GetVector("left", "right", "down", "up");
-		if(!IsDashing){
-			if(inputDirection.X != 0){
-				Direction = (int)inputDirection.X;
-			}
-			Velocity = inputDirection * Speed;
-			/*MoveAndSlide();*/
-		}
-		
-		Dash();
-		Pula();
 	}
 	
 	private void Pula(){
@@ -106,7 +122,7 @@ public partial class Namorado : CharacterBody2D
 	}
 	
 	private void Dash(){
-		if(Input.IsActionJustPressed("dash")){
+		if(Input.IsActionJustPressed("dash") && !IsDashing && !IsPunching){
 			IsDashing = true;
 			TimerDash.Start();
 		}
@@ -124,5 +140,24 @@ public partial class Namorado : CharacterBody2D
 	private void FimDoDash(){
 		IsDashing = false;
 		Gravity = GravNum;
+	}
+	
+	private void Bate(){
+		if(Input.IsActionJustPressed("soco") && !IsPunching && !IsDashing){
+			IsPunching = true;
+			Sprite.Frame = 1;
+			Soco.Monitoring = true;
+			TimerSoco.Start();
+		}
+	}
+	
+	private void FimDoSoco(){
+		IsPunching = false;
+		Sprite.Frame = 0;
+		Soco.Monitoring = false;
+	}
+	
+	private void SocoColisao(Node2D bodyx){
+		GD.Print("Namorado acaba de executar um soco!");
 	}
 }
